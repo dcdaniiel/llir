c55c0da16e72922a47e752b756a36cebcced58e1
Daniel Teixeira <dc.daniiel@gmail.com>
2020-12-29 01:07:53 -0300

    base config to API

--- src/api/controllers/auth.js
+++ src/api/controllers/auth.js
@@ -7,10 +7,13 @@ module.exports = () => {
   return {
     async create(ctx) {
       try {
-        const { body } = ctx.body;
+        const { body } = ctx.request.body;
 
-        ctx.boxy = await user.create(body);
+        console.log('BODY:::', body);
+        ctx.body = await user.create(body);
+        ctx.status = 201;
       } catch (e) {
+        console.log('ERROR::', e);
         ctx.body = e.errors || e.detail;
         ctx.status = 400;
       }
